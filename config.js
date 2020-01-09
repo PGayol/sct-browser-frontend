9aa338cfa4588270d56c0f101773c35333b94217
Patricio Gayol <patricio@termmed.com>
2020-01-09 17:20:00 -0300

    minor fixes

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config = { //URU
     langRefset: ["5641000179103"],
     statsButtons: ["AAAAAA", "BBBBBB"], // URI of each statistics page 
     statsButtonsText:["Link to Statistics 1", "Link to Statistics 2"], // Description text of each button
-    selectedLanguage: "es_UY",
+    selectedLanguage: "en_US",
     selectedFlag: "img/flags/uy.png",
     selectedRightLogo: "img/logo-salud-uy-small.png",
     selectedLeftLogo: "",
