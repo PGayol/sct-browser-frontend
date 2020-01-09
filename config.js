41ff7e12264b871de54e82ac46817d61b874968a
Patricio Gayol <patricio@termmed.com>
2020-01-09 16:11:12 -0300

    first changes adapting config variables

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = { //URU
+    serverUrl: "",
+    edition: "",
+    release: "",
+    releaseName: "",
+    langRefset: ["5641000179103"],
+    statsButtons: 0, // Set to 0, 1 or 2 depending on how many statistics buttons you want to show. E.g: 2 for Cadanada (extension and french translation stats).
+    selectedLanguage: "es_UY",
+    selectedFlag: "img/flags/uy.png",
+    selectedLogo: "img/logo-salud-uy-small.png",
+    copyrightText: "&copy; Salud.uy 2019",
+    bussinesLink: "https://Salud.uy",
+    languages: ["es"],
+    languagesArray: {
+        'es': 'Spanish'
+    },
+}
